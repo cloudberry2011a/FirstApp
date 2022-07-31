e1ddfc6485cf244ec5ba1846de3ff4c9fd88ea41
Kendall Chua <cloudberry2022@icloud.com>
2022-07-31 08:39:17 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kendall Chua on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
